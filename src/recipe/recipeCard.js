a5d1d66a20644735bf1c28c3077b272d2f9d2289
rui jing <jingrui5@163.com>
2024-01-25 10:55:42 +0000

    add recipe card into the recipe page.

---
+++ src/recipe/recipeCard.js
@@ -0,0 +1,39 @@
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+import "./recipeCard.css";
+import {GetRecipes} from "../utils";
+
+function RecipeCardList() {
+    const recipes = GetRecipes();
+    return <div className="wrapper">
+
+        {recipes.map((recipe) => {
+
+            return (
+
+                <Card
+                    img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
+                    title={recipe.name}
+                    description="Take your boring salads up a knotch. This recipe is perfect for lunch
+      and only contains 5 ingredients!"
+                />
+
+            )
+        })}
+
+    </div>;
+}
+
+function Card(props) {
+    return (
+        <div className="card">
+            <div className="card__body">
+                <img src={props.img} class="card__image"/>
+                <h2 className="card__title">{props.title}</h2>
+                <p className="card__description">{props.description}</p>
+            </div>
+            <button className="card__btn">View Recipe</button>
+        </div>
+    );
+}
+
+export default RecipeCardList;
