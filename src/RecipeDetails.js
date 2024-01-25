41f3ed0c096a108862f94755b848938a40862143
Hannah Ponting <hannahponting01@gmail.com>
2024-01-25 16:45:34 +0000

    Added css to recipe detail page

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -1,51 +1,58 @@
 import { Await, useParams } from "react-router-dom"
 import { GetRecipes, GetRecipesById } from "./utils";
+import "./RecipeDetails.css";
 
 
 function RecipeDetails(props) {
-    let params = useParams()
-    const recipes = props.recipes;
-    return <div className="wrapper">
-
-        {recipes.map((recipe) => {
-            if (recipe.id == params.id)
-            
+    let params = useParams();
+    try{
+    let recipe = GetRecipesById(params.id);
             return (
-        <div>
-                <div className="recipe-details-page">
-                    <h1>{recipe.name}</h1>
-                     <img src={`http://localhost:8080/api/recipes/image/${recipe.id}`}class="card__image"/>
+        <div className="details-page">
+                <h1 className="heading">{recipe.name}</h1>
+                <div className="details">
+            
+                     <img src={`http://localhost:8080/api/recipes/image/${recipe.id}`}class="details_image"/>
                         <h2>Details</h2>
-                        <ul className="recipe-details">
+                        <ul className="properties">
+                        <li><img src={require('./icons8-clock-100 1.png')} className="timer"/>
+                            <strong> &nbsp;Time to cook: </strong>{recipe.time_to_cook}</li>
                             <li><strong>Name: </strong>{recipe.name}</li>
                             <li><strong>Meal Type: </strong>{recipe.mealType[0] + recipe.mealType.slice(1).toLowerCase()}</li>
                             <li><strong>Cuisine: </strong>{recipe.cuisine[0] + recipe.cuisine.slice(1).toLowerCase()}</li>
                             <li><strong>Serving: </strong>{recipe.serving}</li>
-                            <li><strong>Time to cook: </strong>{recipe.time_to_cook}</li>
                             <li><strong>Difficulty Level: </strong>{recipe.difficulty_level[0] + recipe.difficulty_level.slice(1).toLowerCase()}</li>
                             <li><strong>Cost: </strong>{recipe.cost[0] + recipe.cost.slice(1).toLowerCase()}</li>
                             <li><strong>Spice level: </strong>{recipe.spice_level[0] + recipe.spice_level.slice(1).toLowerCase()}</li>
                         </ul>
-                        </div>
-                        <h2>Ingredients: </h2>
+                 
+                        <div className="boxes">
+                        <h2>Ingredients </h2>
                         <ul className="recipe-ingredients">
                             {recipe.ingredients.map((ingredient, index) => {
                             return <li key="{index}">{ingredient}</li>})}
                         </ul>
-                        <h2>Instructions:</h2>
+                        </div>
+                        <div className="boxes">
+                        <h2>Instructions</h2>
                         <ul className="recipe-instructions">
                             {recipe.instructions.map((ingredient, index) => {
                             return <li key="{index}">{ingredient}</li>})}
                         </ul>
+                        </div>
+                        </div>
 
             
-             
-                </div>
+             </div>
+            );
+                            }
+                            catch(err){
+                                return(<div>
+                                    <p>error</p>
+                                </div>)
+                            }
+      
 
-            )
-        })}
-
-    </div>;
 }
 
 export default RecipeDetails;
