c429c5a08afe0a39529bb7338ce3bf9d1bf3e52e
Edmund Woodfield <edmundwoodfield@gmail.com>
2024-02-02 12:06:47 +0000

    filter implementation merged into recipe page search, fetch function simplified

--- src/filterBar/RecipeFilter.js
+++ src/filterBar/RecipeFilter.js
@@ -76,72 +76,72 @@ export const RecipeFilter = ({ applyFilters }) => {
 
 };
 
-export const RecipeList = ({ recipes }) => {
-    return (
-        <>
+// export const RecipeList = ({ recipes }) => {
+//     return (
+//         <>
 
 
 
-            < div className="wrapper" >
+//             < div className="wrapper" >
 
-                {recipes.length === 0 ? (
-                    <p>No recipes for this filter combination found</p>
-                ) : (
-                    recipes.map((recipe) => (
-                        <>
+//                 {recipes.length === 0 ? (
+//                     <p>No recipes for this filter combination found</p>
+//                 ) : (
+//                     recipes.map((recipe) => (
+//                         <>
     
-                            <Card
-                                key={recipe.id}
-                                img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
-                                title={recipe.name}
-                                description="Take your boring salads up a knotch. This recipe is perfect for lunch and only contains 5 ingredients!"
-                                id={recipe.id}
-                            />
-                        </>
-                    ))
-                )}
-            </div>
+//                             <Card
+//                                 key={recipe.id}
+//                                 img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
+//                                 title={recipe.name}
+//                                 description="Take your boring salads up a knotch. This recipe is perfect for lunch and only contains 5 ingredients!"
+//                                 id={recipe.id}
+//                             />
+//                         </>
+//                     ))
+//                 )}
+//             </div>
 
-        </>
-    );
-};
-
-
-
-export default function RecipeFilterApp({ setFilterStatus }) {
-
-    const [filteredRecipes, setFilteredRecipes] = useState([]);
-    const [filterButtonStatus, setFilterButtonStatus] = useState("off");
-    const applyFilters = (costLevel, difficultyLevel, spiceLevel) => {
-
-        fetch(`http://localhost:8080/api/recipes`)
-            .then(response => response.json())
-            .then(data => {
-                let newdata = data.filter(recipe => {
-                    return recipe["cost"].includes(costLevel) && recipe["difficulty_level"].includes(difficultyLevel) && recipe["spice_level"].includes(spiceLevel)
-
-                });
-
-                setFilteredRecipes(newdata);
-                setFilterButtonStatus('on');
-                setFilterStatus('on');
+//         </>
+//     );
+// };
 
 
-            })
 
-    }
+// export default function RecipeFilterApp({ setFilterStatus }) {
 
-    return (
-        <>
+//     const [filteredRecipes, setFilteredRecipes] = useState([]);
+//     const [filterButtonStatus, setFilterButtonStatus] = useState("off");
+//     const applyFilters = (costLevel, difficultyLevel, spiceLevel) => {
 
-            <div>
-                <RecipeFilter applyFilters={applyFilters} />
-                {filterButtonStatus === "on" && <RecipeList recipes={filteredRecipes} />}
-            </div>
-        </>
+//         fetch(`http://localhost:8080/api/recipes`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 let newdata = data.filter(recipe => {
+//                     return recipe["cost"].includes(costLevel) && recipe["difficulty_level"].includes(difficultyLevel) && recipe["spice_level"].includes(spiceLevel)
 
-    )
-};
+//                 });
+
+//                 setFilteredRecipes(newdata);
+//                 setFilterButtonStatus('on');
+//                 setFilterStatus('on');
+
+
+//             })
+
+//     }
+
+//     return (
+//         <>
+
+//             <div>
+//                 <RecipeFilter applyFilters={applyFilters} />
+//                 {filterButtonStatus === "on" && <RecipeList recipes={filteredRecipes} />}
+//             </div>
+//         </>
+
+//     )
+// };
 
 
 
--- src/recipeCards/recipeCard.js
+++ src/recipeCards/recipeCard.js
@@ -3,97 +3,65 @@ import "./recipeCard.css";
 import { GetRecipesPaginated } from "../utils";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import RecipeFilterApp, {RecipeFilter} from "../filterBar/RecipeFilter";
-
-import {RecipeList} from "../filterBar/RecipeFilter";
+import {RecipeFilter} from "../filterBar/RecipeFilter";
 
 
 function RecipeCardList() {
 
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
-
-    const [filterStatus, setFilterStatus] = useState("off");
-
-
-
     const handleSearch = () => {
         navigate(`/recipes/search?keyword=${encodeURIComponent(searchTerm)}`)
     }
 
     const [recipes, setRecipes] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    let data = GetRecipesPaginated(currentPage, 10);
-    const fetchedRecipes = data.recipes;
+    const [query, setQuery] = useState("");
     const [totalPages, setTotalPages] = useState(1);
 
     useEffect(() => {
-        setRecipes(fetchedRecipes);
-    }, [currentPage, data]);
-    useEffect(() => {
-        setTotalPages(data.totalPages);
-    }, [data]);
+        const fetchData = async () => {
+            const result = await GetRecipesPaginated(currentPage, 10, query);
+            console.log(result);
+            setRecipes(result.recipes);
+            setTotalPages(result.totalPages);
+        };
+    
+        fetchData();
+    }, [currentPage, query]);
+
     const handlePageChange = (newPage) => {
         if (newPage > 0 && newPage <= totalPages) {
             setCurrentPage(newPage);
         }
     };
 
-
-
-    // const [filteredRecipes, setFilteredRecipes] = useState([]);
-    // const [filterButtonStatus, setFilterButtonStatus] = useState("off");
-    // const applyFilters = (costLevel, difficultyLevel, spiceLevel) => {
-    //
-    //     fetch(`http://localhost:8080/api/recipes`)
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             let newdata = data.filter(recipe => {
-    //                 return recipe["cost"].includes(costLevel) && recipe["difficulty_level"].includes(difficultyLevel) && recipe["spice_level"].includes(spiceLevel)
-    //
-    //             });
-    //
-    //             setFilteredRecipes(newdata);
-    //             setFilterButtonStatus('on');
-    //             setFilterStatus('on');
-    //
-    //
-    //         })
-    //
-    // }
-
-    const [filteredRecipes, setFilteredRecipes] = useState([]);
-    const [filterButtonStatus,setFilterButtonStatus] = useState("off");
     const applyFilters = (costType,difficultyLevel, spiceLevel) => {
-        const page = 1;
-        const pageSize = 10;
-        let apiUrl="";
 
         if (costType===""&&difficultyLevel===""&&spiceLevel==="")
-            apiUrl=`http://localhost:8080/api/recipes/search/custom/page/${page}/${pageSize}`
+           {setQuery("");
+           setCurrentPage(1);
+        console.log("empty query " + query)}
         else
-            apiUrl =
-                `http://localhost:8080/api/recipes/search/custom/page/${page}/${pageSize}?query=difficultyLevel%3D${difficultyLevel}%26costType%3D${costType}%26spiceType%3D${spiceLevel}`;
-
-
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-
-                console.log("Fetched data:", data.content);
-                setFilteredRecipes(data.content);
-                setFilterButtonStatus('on');
-                setFilterStatus('on');
-
-            })
-            .catch(error => {
-                // Handle errors if any occurred during the fetch
-                console.error("Error fetching data:", error);
-            });
-
-
+        {
+        let queryParams = "";
+    
+        if (costType !== "") {
+            queryParams += `costType=${costType}`;
+        }
+    
+        if (difficultyLevel !== "") {
+            queryParams += `${queryParams.length > 0 ? '&' : ''}difficultyLevel=${difficultyLevel}`;
+        }
+    
+        if (spiceLevel !== "") {
+            queryParams += `${queryParams.length > 0 ? '&' : ''}spiceType=${spiceLevel}`;
+        }
+    
+        setQuery("?query="+encodeURIComponent(queryParams));
+        setCurrentPage(1);
     }
-
+};
 
 
     return <>
@@ -123,54 +91,47 @@ function RecipeCardList() {
 
 
 
-        <div className="Divider" ></div>
+        <div className="Divider"></div>
 
+{recipes.length > 0 ? (
+    <div className="wrapper">
+        {recipes.map((recipe) => (
+            <Card
+                key={recipe.id}
+                img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
+                title={recipe.name}
+                description="Take your boring salads up a notch. This recipe is perfect for lunch and only contains 5 ingredients!"
+                id={recipe.id}
+            />
+        ))}
+    </div>
+) : (
+    <div className="no-recipes-message">
+        No recipes to display. Try adjusting your filters or searching with different keywords.
+    </div>
+)}
 
-        {filterStatus === "off" ?
-
-            <>
-            <div className="wrapper">
-
-                {recipes.map((recipe) => {
-                    return (
-                        <Card
-                            key={recipe.id}
-                            img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
-                            title={recipe.name}
-                            description="Take your boring salads up a knotch. This recipe is perfect for lunch
-      and only contains 5 ingredients!"
-                            id={recipe.id}
-                        />
-                    )
-                })}
-
-            </div>
-
-
-            <div className="button-container">
-                <div>
-                    <button className="buttonEditing" onClick={() => handlePageChange(currentPage - 1)}
-                            disabled={currentPage === 1}>
-                        Previous Page
-                    </button>
-                    &nbsp;&nbsp;&nbsp;
-                    <button className="buttonEditing" onClick={() => handlePageChange(currentPage + 1)}
-                            disabled={currentPage === totalPages}>
-                        Next Page
-                    </button>
-                </div>
-
-            </div>
-            </>
-
-
-            :<div>
-                {filterButtonStatus === "on" && <RecipeList recipes={filteredRecipes} />}
-            </div>}
-
-
-    </>;
-}
+<div className="button-container">
+    <div>
+        <button
+            className="buttonEditing"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+        >
+            Previous Page
+        </button>
+        &nbsp;&nbsp;&nbsp;
+        <button
+            className="buttonEditing"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+        >
+            Next Page
+        </button>
+    </div>
+</div>
+</>
+;}
 
 export function Card(props) {
     let link = "/recipes/" + props.id;
