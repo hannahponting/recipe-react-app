69fc511a9a5a86133da2aff328d045c66a6f009c
Hannah Ponting <hannahponting01@gmail.com>
2024-02-02 14:30:23 +0000

    Added log out button

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,18 +9,6 @@ import { GetUserByEmail } from "./utils";
 
 
 const Navigation = (props) => {
-
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const handleLogin = () => {
-        
-        
-        setIsLoggedIn(true);
-
-    };
-
-    
-
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
 
@@ -29,13 +17,19 @@ const Navigation = (props) => {
         navigate(`/recipes/search?keyword=${encodeURIComponent(searchTerm)}`)
     }
 
-
+    const handleLogOut = () => {
+        props.setIsLoggedIn(false);
+    }
 
         return (
             <>
                 <nav className="navigation">
-                    <h3>Hello {props.userID}</h3>
-
+                         {props.isLoggedIn ? (
+                                 <h3>Hello {props.userID}</h3>
+                        ) : (
+                            <h3>Hello Guest</h3>
+                        )}
+                   
                     <ul className="itemsAligned">
                         <li>
                             <img className="nerd-logo-removebg" alt="Nerd logo" src={require('.//Resources/invertedNerdLogo.png')}></img>
@@ -54,12 +48,14 @@ const Navigation = (props) => {
                         <li>
                             <Link className="hyperlink" to="/WhoWeAre">Who We Are</Link>
                         </li>
-                        {isLoggedIn ? (
+                        <li> <Link className="hyperlink" to ="/favourites">Favorites</Link></li>
+                        {props.isLoggedIn ? (
                             <li>
-                                <Link className="hyperlink" to ="/favourites">Favorites</Link></li>
+                                <Link className="hyperlink" to="/" onClick={handleLogOut}>Log Out</Link></li>
                         ) : (
-                            <li><Link className="hyperlink" onClick={handleLogin} to = "/login">Log In</Link></li>
+                            <li><Link className="hyperlink" to = "/login">Log In</Link></li>
                         )}
+                        
 
                     </ul>
 
