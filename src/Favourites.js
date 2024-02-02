c33bc5d13d727d5878550563d5e16b154b99e7d3
rui jing <jingrui5@163.com>
2024-02-02 13:18:43 +0000

    get recipe card page with filter and search work with pages and refactor the code.

--- src/Favourites.js
+++ src/Favourites.js
@@ -4,116 +4,61 @@ import './whoWeAre.css';
 // import {View, Text} from 'react-native';
 import React, {useEffect, useState} from "react";
 import {GetRecipesPaginated} from "./utils";
-import {Card} from "./recipeCards/recipeCard";
 const Favourites = (props) => {
+    const navigate = useNavigate()
 
-
-    const [recipes, setRecipes] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await GetUserFavourRecipes(1, 5, props.uuID);
-            setRecipes(result.recipes);
-            setTotalPages(result.totalPages);
-        };
-        console.log("print out recipes below")
-        console.log(recipes)
-
-        fetchData();
-    }, [currentPage]);
-
-    const handlePageChange = (newPage) => {
-        if (newPage > 0 && newPage <= totalPages) {
-            setCurrentPage(newPage);
-        }
-    };
-
+    const whoWeAreText = "Welcome to NERD Recipes where passion meets practicality in the world of culinary delights! <br /> We are a small but dedicated team committed to revolutionizing your cooking experience. <br /> At the heart of our endeavor is a recipe website that goes beyond the ordinary, championing ad-free and affordable culinary inspiration while providing unparalleled recipe searching capabilities.<br /> Who are we? <br /> We're food enthusiasts with a mission to make cooking accessible, enjoyable, and budget-friendly for everyone. In a world cluttered with distractions, we believe in a seamless and ad-free recipe browsing experience that lets you focus on what truly matters the joy of creating delicious meals. <br /> Our commitment to affordability is embedded in every aspect of our platform. From cost-effective ingredients to budget-friendly meal plans, we empower you to savor the flavors without breaking the bank. We understand the importance of a diverse and accessible recipe collection, and we take pride in curating a vast array of dishes that cater to different tastes, preferences, and dietary needs. <br /> What sets us apart? <br />It is our dedication to intuitive recipe searching. Our website is designed to be your culinary companion, offering powerful search capabilities that simplify the quest for the perfect recipe. Whether you're a seasoned chef or a kitchen novice, our user-friendly interface ensures that you find exactly what you're looking for, effortlessly. <br />Join us in embracing a community where cooking is a delightful adventure, unburdened by intrusive ads and excessive costs. At NERD Recipe, we're not just offering recipes; we're inviting you to be part of a movement that values affordable, ad-free, and exceptional culinary experiences. Let's cook up something extraordinary together!"
 
     return (
         <>
-        <div>
-        {props.isLoggedIn ?  (
-        <>
             <header className="header">
-
-                <div className="Title">Find your favourite recipes here</div>
-
+                <div className="Title">Want to display my favourite recipes here</div>
             </header>
 
             <div className="Divider"></div>
 
-
-            {recipes.length > 0 ? (
-                <div className="wrapper">
-                    {recipes.map((recipe) => (
-                        <Card
-                            key={recipe.id}
-                            img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
-                            title={recipe.name}
-                            description="Take your boring salads up a notch. This recipe is perfect for lunch and only contains 5 ingredients!"
-                            id={recipe.id}
-                        />
-                    ))}
-                </div>
-            ) : (
-                <div className="no-recipes-message">
-                    No recipes to display.
-                </div>
-            )}
-
-            <div className="button-container">
-                <div>
-                    <button
-                        className="buttonEditing"
-                        onClick={() => handlePageChange(currentPage - 1)}
-                        disabled={currentPage === 1}
-                    >
-                        Previous Page
-                    </button>
-                    &nbsp;&nbsp;&nbsp;
-                    <button
-                        className="buttonEditing"
-                        onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage === totalPages}
-                    >
-                        Next Page
-                    </button>
-                </div>
+            <div>
+                <img className= "photo" src={require('.//cooker.png')} alt="Cooker" />
             </div>
 
-
-
         </>
-    )
-            :(
-            <>
-                <header className="header">
-                    <div className="Title">Want to display my favourite recipes here</div>
-                </header>
-
-                <div className="Divider"></div>
-
-
-                {/*<div><img className= "photo" src={require('.//cooker.png')} alt="Cooker" /></div>*/}
-            </>
-        ) }
-
-    </div>
-    </>
-    )
-            
-};
-
-
-export async function GetUserFavourRecipes(pageNum, pageSize, userId) {
-    const urlApi = `http://localhost:8080/api/recipes/favourite/${userId}/page/${pageNum}/${pageSize}`
-    const response = await fetch(urlApi);
-    const body = await response.json()
-    const data = ({recipes: body.content, isLoading: false, totalPages: body.totalPages})
-    return data;
+    );
 }
 
-
 export default Favourites;
+
+
+
+
+function RenderUserFavorRecipes() {
+
+    const navigate = useNavigate();
+    const apiUrl ='http://localhost:8080/api/recipes/favourite/2/page/1/10'
+
+    const handleSearch = () => {
+        navigate(apiUrl)
+    }
+
+    const [recipes, setRecipes] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    let data = GetRecipesPaginated(currentPage, 10);
+
+    // console.log(data)
+    const fetchedRecipes = data.recipes;
+    const [totalPages, setTotalPages] = useState(1);
+
+    useEffect(() => {
+        setRecipes(fetchedRecipes);
+    }, [currentPage, data]);
+    useEffect(() => {
+        setTotalPages(data.totalPages);
+    }, [data]);
+    const handlePageChange = (newPage) => {
+        if (newPage > 0 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        }
+        ;
+
+    };
+    return {handleSearch, recipes, currentPage, totalPages, handlePageChange};
+}
--- src/recipeCards/recipeCard.js
+++ src/recipeCards/recipeCard.js
@@ -3,149 +3,224 @@ import "./recipeCard.css";
 import { GetRecipesPaginated } from "../utils";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {RecipeFilter} from "../filterBar/RecipeFilter";
-import LikeButton from "../likeButton/likeButtton";
+import RecipeFilterApp, {RecipeFilter} from "../filterBar/RecipeFilter";
 
-function RecipeCardList(props) {
+import {RecipeList} from "../filterBar/RecipeFilter";
 
+
+function RecipeCardList() {
+
+    const {searchTerm, setSearchTerm, handleSearch, recipes,
+        currentPage, totalPages, handlePageChange} = ExtractedSearchApp();
+    const {
+        filterStatus, filteredRecipes, filterButtonStatus, totalPageForFilter,
+        currentPageForFilter, applyFilters, handlePageChangeForFilter} = ExtractedFileterApp();
+
+    return <>
+
+            <header className="header">
+                 <div className="Title">Recipes</div>
+                 <RecipeFilter applyFilters={applyFilters}/>
+                {SearchBar(searchTerm, setSearchTerm, handleSearch)}
+            </header>
+
+        <div className="Divider"></div>
+
+        {filterStatus === "off" ?
+            <>
+                {getRecipeCards(recipes)}
+                {getPage(handlePageChange, currentPage, totalPages)}
+            </>
+            :
+            <>
+                {filterButtonStatus === "on" && <RecipeList recipes={filteredRecipes}/>}
+                {getPageForFilter(handlePageChangeForFilter, currentPageForFilter, totalPageForFilter)}
+            </>
+
+        }
+
+    </>;
+}
+
+
+
+
+function ExtractedFileterApp() {
+    const [filterStatus, setFilterStatus] = useState("off");
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [filterButtonStatus, setFilterButtonStatus] = useState("off");
+    const [totalPageForFilter, setTotalPageForFilter] = useState(1);
+    const [currentPageForFilter, setCurrentPageForFilter] = useState(1);
+    const [costType, setCostType] = useState("Select All");
+    const [difficultyLevel, setDifficultyLevel] = useState("Select All");
+    const [spiceLevel, setSpiceLevel] = useState("Select All");
+
+    console.log("currentPageForFilter not in handle page" + currentPageForFilter)
+    const handlePageChangeForFilter = (newPage) => {
+        if (newPage > 0 && newPage <= totalPageForFilter) {
+            setCurrentPageForFilter(newPage)
+        }
+
+        applyFilters(costType, difficultyLevel, spiceLevel, newPage);
+
+    };
+
+    const applyFilters = (costType, difficultyLevel, spiceLevel, currentPageForFilter) => {
+
+        const pageSize = 10;
+        let apiUrl = "";
+
+        if (costType === "" && difficultyLevel === "" && spiceLevel === "")
+            apiUrl = `http://localhost:8080/api/recipes/search/custom/page/${currentPageForFilter}/${pageSize}`
+        else
+            apiUrl =
+                `http://localhost:8080/api/recipes/search/custom/page/${currentPageForFilter}/${pageSize}?query=difficultyLevel%3D${difficultyLevel}%26costType%3D${costType}%26spiceType%3D${spiceLevel}`;
+
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+
+                setFilteredRecipes(data.content);
+                setFilterButtonStatus('on');
+                setFilterStatus('on');
+                setTotalPageForFilter(data.totalPages)
+                setCostType(costType);
+                setDifficultyLevel(difficultyLevel);
+                setSpiceLevel(spiceLevel);
+
+
+            })
+            .catch(error => {
+                // Handle errors if any occurred during the fetch
+                console.error("Error fetching data:", error);
+            });
+
+    }
+    return {
+        filterStatus,
+        filteredRecipes,
+        filterButtonStatus,
+        totalPageForFilter,
+        currentPageForFilter,
+        applyFilters,
+        handlePageChangeForFilter
+    };
+}
+
+
+function getPageForFilter(handlePageChangeForFilter, currentPageForFilter, totalPageForFilter) {
+    return <div className="button-container">
+        <div>
+            <button className="buttonEditing" onClick={() => {
+                handlePageChangeForFilter(currentPageForFilter - 1)
+            }}
+                    disabled={currentPageForFilter === 1}>
+                Previous Page
+            </button>
+            &nbsp;&nbsp;&nbsp;
+            <button className="buttonEditing" onClick={() => {
+                handlePageChangeForFilter(currentPageForFilter + 1);
+            }}
+                    disabled={currentPageForFilter === totalPageForFilter}>
+                Next Page
+            </button>
+        </div>
+    </div>;
+}
+
+
+
+function SearchBar(searchTerm, setSearchTerm, handleSearch) {
+    return <div className="search-bar">
+        <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+    </div>;
+}
+
+
+
+
+ function ExtractedSearchApp() {
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
+
     const handleSearch = () => {
         navigate(`/recipes/search?keyword=${encodeURIComponent(searchTerm)}`)
     }
 
     const [recipes, setRecipes] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [query, setQuery] = useState("");
+    let data = GetRecipesPaginated(currentPage, 10);
+
+    // console.log(data)
+    const fetchedRecipes = data.recipes;
     const [totalPages, setTotalPages] = useState(1);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const result = await GetRecipesPaginated(currentPage, 10, query);
-            setRecipes(result.recipes);
-            setTotalPages(result.totalPages);
-        };
-    
-        fetchData();
-    }, [currentPage, query]);
-
+        setRecipes(fetchedRecipes);
+    }, [currentPage, data]);
+    useEffect(() => {
+        setTotalPages(data.totalPages);
+    }, [data]);
     const handlePageChange = (newPage) => {
         if (newPage > 0 && newPage <= totalPages) {
             setCurrentPage(newPage);
         }
+        ;
+
     };
-
-    const applyFilters = (costType,difficultyLevel, spiceLevel) => {
-
-        if (costType===""&&difficultyLevel===""&&spiceLevel==="")
-           {setQuery("");
-           setCurrentPage(1);
-        }
-        else
-        {
-        let queryParams = "";
-    
-        if (costType !== "") {
-            queryParams += `costType=${costType}`;
-        }
-    
-        if (difficultyLevel !== "") {
-            queryParams += `${queryParams.length > 0 ? '&' : ''}difficultyLevel=${difficultyLevel}`;
-        }
-    
-        if (spiceLevel !== "") {
-            queryParams += `${queryParams.length > 0 ? '&' : ''}spiceType=${spiceLevel}`;
-        }
-    
-        setQuery("?query="+encodeURIComponent(queryParams));
-        setCurrentPage(1);
-    }
-};
+    return {searchTerm, setSearchTerm, handleSearch, recipes, currentPage, totalPages, handlePageChange};
+}
 
 
-    return <div className="recipecard-page-container">
 
+function getPage(handlePageChange, currentPage, totalPages) {
+    return <div className="button-container">
         <div>
-            <header className="header">
-                <div className="Title">
-                    Recipes
-                </div>
-                <div>
-                    <RecipeFilter applyFilters={applyFilters} />
-                </div>
-                <div className="search-bar">
-                    <input
-                        type="text"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <button onClick={handleSearch}>Search</button>
-                </div>
+            <button className="buttonEditing" onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}>
+                Previous Page
+            </button>
+            &nbsp;&nbsp;&nbsp;
+            <button className="buttonEditing" onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}>
+                Next Page
+            </button>
+        </div>
 
-            </header>
+    </div>;
+}
 
-        {/* {recipes.map((recipe) => {
+
+
+function getRecipeCards(recipes) {
+    return <div className="wrapper">
+
+        {recipes.map((recipe) => {
             return (
                 <Card
                     key={recipe.id}
                     img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
                     title={recipe.name}
-                    description={"Delicious recipe from " + recipe.cuisine.toLowerCase() + " cuisine. It serves up to " + recipe.serving + " people!" }
+                    description="Take your boring salads up a knotch. This recipe is perfect for lunch
+      and only contains 5 ingredients!"
                     id={recipe.id}
                 />
             )
-        })} */}
+        })}
 
+    </div>;
+}
 
-        </div>
-        <div className="Divider"></div>
-
-{recipes.length > 0 ? (
-    <div className="wrapper">
-        {recipes.map((recipe) => (
-            <Card
-                key={recipe.id}
-                img={`http://localhost:8080/api/recipes/image/${recipe.id}`}
-                title={recipe.name}
-                description={"Delicious recipe from " + recipe.cuisine.toLowerCase() + " cuisine. It serves up to " + recipe.serving + " people!" }
-                id={recipe.id}
-                isLoggedIn={props.isLoggedIn}
-                uuId ={props.uuID}
-            />
-        ))}
-    </div>
-) : (
-    <div className="no-recipes-message">
-        No recipes to display. Try adjusting your filters or searching with different keywords.
-    </div>
-)}
-
-<div className="button-container">
-    <div>
-        <button
-            className="buttonEditing"
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-        >
-            Previous Page
-        </button>
-        &nbsp;&nbsp;&nbsp;
-        <button
-            className="buttonEditing"
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-        >
-            Next Page
-        </button>
-    </div>
-</div>
-</div>
-;}
 
 export function Card(props) {
     let link = "/recipes/" + props.id;
     return (
-
         <div className="card">
             <div className="card__body">
                 <img src={props.img} className="card__image"/>
@@ -155,10 +230,13 @@ export function Card(props) {
             <Link to={link}>
                 <button className="card__btn">View Recipe</button>
             </Link>
-            <LikeButton isUserLoggedIn={props.isLoggedIn}  recipeId={props.id}  uuId ={props.uuId}> </LikeButton>
         </div>
-
     );
 }
+
+
+
+
 export default RecipeCardList;
+
 
