f10b6888420c342745a1fb630034205d2f1908c8
Maria O Mullony <mschzomullony@gmail.com>
2024-02-05 13:18:13 +0000

    footer edits

--- src/component/NavBar/DropdownAccount.js
+++ src/component/NavBar/DropdownAccount.js
@@ -1,6 +1,5 @@
 import { Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./DropDownAccount.css";
 
 const DropdownAccount = (props) => {
 
@@ -8,20 +7,19 @@ const DropdownAccount = (props) => {
         props.setIsLoggedIn(false);
         props.setUserId('');
     }
-    return (
-        
-            <div className="dropdown-menu">
-                <Dropdown>
-                    <Dropdown.Toggle varient="success" id="dropdown-basic">
-                        Hello {props.userID}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item as={Link} to="/favourites">Favourites</Dropdown.Item>
-                        <Dropdown.Item as={Link} to="/changepassword">Change Password</Dropdown.Item>
-                        <Dropdown.Item as={Link} onClick={handleLogOut} to="/home">Log Out</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
+
+
+    return(
+        <Dropdown>
+            <Dropdown.Toggle varient="success" id="dropdown-basic">
+                Hello {props.userID}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item as={Link} to="/favourites">Favourites</Dropdown.Item>
+                <Dropdown.Item as={Link} to="/changepassword">Change Password</Dropdown.Item>
+                <Dropdown.Item as={Link} onClick={handleLogOut} to="/home">Log Out</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
     )
 
 }
