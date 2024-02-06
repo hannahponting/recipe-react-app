f02d310923740134c6005caa99d47f92f39d0b74
Maria O Mullony <mschzomullony@gmail.com>
2024-02-06 16:44:53 +0000

    removing from view side bar and like button directs you to login

--- src/component/likeButton/likeButton.js
+++ src/component/likeButton/likeButton.js
@@ -1,14 +1,17 @@
-import React, {useContext, useState,useEffect} from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import "./likeButton.css"
 import AuthContext from "../AuthContext/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 
 
 function LikeButton(props) {
     const context = useContext(AuthContext);
     let user = context.user;
-    console.log("user"+user);
+    console.log("user" + user);
     let personID = user?.id ?? null;
+
+    const navigate = useNavigate();
 
     const [isActive, setIsActive] = useState(false);
 
@@ -20,31 +23,29 @@ function LikeButton(props) {
         if (user) {
             const initialState = await isFavourite(props.recipeId, personID);
             setIsActive(initialState);
-        }};
+        }
+    };
 
     const handleClick = () => {
 
         if (user) {
-            setIsActive(prevState => {return !prevState;});
+            setIsActive(prevState => { return !prevState; });
             postNewChangeToBack(props, personID, isActive);
 
         } else {
 
-
+            navigate('/login');
         }
-    }
-
-        return (
-            <>
-                <div className={`heart-btn ${isActive ? 'heart-active' : ''}`} onClick={handleClick}>
-                    <div className="content">
-
-                        <span className={`heart ${isActive ? 'heart-active' : ''}`}></span>
-                    </div>
+    };
+    return (
+        <>
+            <div className={`heart-btn ${isActive ? 'heart-active' : ''}`} onClick={handleClick}>
+                <div className="content">
+                    <span className={`heart ${isActive ? 'heart-active' : ''}`}></span>
                 </div>
-            </>
-        );
-
+            </div>
+        </>
+    );
 }
 
 
