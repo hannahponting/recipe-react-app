d1e16adc0b2419ca3eb38d65d0e0c093e1c7cb98
Maria O Mullony <mschzomullony@gmail.com>
2024-02-07 11:09:25 +0000

    centering star rating

--- src/component/RateRecipe/RateRecipe.js
+++ src/component/RateRecipe/RateRecipe.js
@@ -3,6 +3,7 @@ import "./RateRecipe.css"
 import { GetUserByEmail } from "../../utils";
 
 import AuthContext from "../AuthContext/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 
 const RateRecipe = (props) => {
@@ -12,65 +13,70 @@ const RateRecipe = (props) => {
   const [rating, setRating] = useState(0);
   const [message, setMessage] = useState("");
   const [hover, setHover] = useState(null);
-  
-    const requestBody = {
-      recipeId: props.id,
-      personId: personID,
-      myRating: rating,
-    };
-  
-    const getData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/api/rating', {
-          method: 'POST',
-          headers: { 'content-type': 'application/json' },
-          body: JSON.stringify(requestBody),
-        });
-        const body = await response.json();
-        if(response.status==201){
+
+  const requestBody = {
+    recipeId: props.id,
+    personId: personID,
+    myRating: rating,
+  };
+  const navigate = useNavigate();
+
+  const getData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/rating', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(requestBody),
+      });
+      const body = await response.json();
+      if (response.status == 201) {
         console.log((body));
         props.fetchData();
         setMessage("Submitted Rating");
-        }
-        if(response.status == 500){
-          console.log(body.message);
-          setMessage(body.message);
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setMessage(error.message)
       }
-    };
-
-    function handleClick(){
-      getData();
-  }
-  
-    return (
-      <div className="star-rating">
-      {[...Array(5)].map((star, index)=> {
-          index += 1;
-          return(
-              <button
-              type ="button"
-              key={index}
-              className={index <= (hover || rating) ? "on" : "off"}
-              onClick={() => setRating(index)}
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={()=> setHover(rating)}
-              >
-                  <span className="star">&#9733;</span>
-              </button>
-              
-          );
-      })}
-      <button className="submit" onClick={handleClick}>Submit</button>
-      <p>{message}</p>
-      </div>
-      
-     
-    );
-
+      if (response.status == 500) {
+        navigate('/login');
+        console.log(body.message);
+        setMessage(body.message);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setMessage(error.message)
+    }
   };
-  
-  export default RateRecipe;
+
+  function handleClick() {
+    getData();
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? "on" : "off"}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="star">&#9733;</span>
+          </button>
+
+        );
+      })}
+
+      <div> <button className="submit" onClick={handleClick}>Submit</button> </div>
+      
+      <p>{message}</p>
+    </div>
+
+
+
+  );
+
+};
+
+export default RateRecipe;
